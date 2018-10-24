d81af74c7c00a7ff1760b323c32a06f89ce451ab
martindg <martindg@gmail.com>
2018-10-24 13:43:47 -0300

    Changes texts

--- app/src/main/java/com/ionnt/navigation/SettingsFragment.kt
+++ app/src/main/java/com/ionnt/navigation/SettingsFragment.kt
@@ -1,13 +1,14 @@
 package com.ionnt.navigation
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-
+/**
+ * Created by Martin De Girolamo on 19/10/2018.
+ */
 class SettingsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
